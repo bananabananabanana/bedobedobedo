ff7ae2bd8ccf50e3e7306117761ee7b9c8aed497
milomory <antondn@mail.ru>
2015-04-13 17:50:22 +0300

    add credit

--- src/main/java/rf/vdolgu/model/User.java
+++ src/main/java/rf/vdolgu/model/User.java
@@ -6,7 +6,29 @@ package rf.vdolgu.model;
 public class User {
     private Integer id;
     private String name;
-    
 
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User() {
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
 }
