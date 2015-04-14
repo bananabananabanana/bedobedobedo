c625b4e601873824c460ce5a3eccba520fda4cc4
Administrator <admin@example.com>
2015-04-15 01:16:51 +0400

    add getCode(@RequestParam("code") String code)

--- src/main/java/rf/vdolgu/controller/MainController.java
+++ src/main/java/rf/vdolgu/controller/MainController.java
@@ -3,6 +3,7 @@ package rf.vdolgu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -10,14 +11,18 @@ public class MainController {
 
     public MainController(){
         System.out.println("MainController");
-        
     }
+
 
     @RequestMapping(method = RequestMethod.GET)
     public String index() {
 
-        String URL = "";
+        return "index";
+    }
 
+    @RequestMapping(method = RequestMethod.GET, params="code")
+    public String getCode(@RequestParam("code") String code) {
+        System.out.println(code);
         return "index";
     }
 
