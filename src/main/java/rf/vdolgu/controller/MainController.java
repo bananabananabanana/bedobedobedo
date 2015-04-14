47d4467440ccaa3700e0849d28002722c276e131
Administrator <admin@example.com>
2015-04-15 01:29:26 +0400

    i get {"access_token":"c68fa970ef1f098393374cc701fcf5855f90174d3cd9a6bb763c0f5b8197c68830b363c53deeb7658671d","expires_in":0,"user_id":53321}

--- src/main/java/rf/vdolgu/controller/MainController.java
+++ src/main/java/rf/vdolgu/controller/MainController.java
@@ -5,9 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @Controller
 @RequestMapping("/")
 public class MainController {
+
+    private final String USER_AGENT = "Mozilla/5.0";
 
     public MainController(){
         System.out.println("MainController");
@@ -21,9 +28,53 @@ public class MainController {
     }
 
     @RequestMapping(method = RequestMethod.GET, params="code")
-    public String getCode(@RequestParam("code") String code) {
+    public String getCode(@RequestParam("code") String code) throws Exception {
         System.out.println(code);
+        if (code != null) {
+            try {
+                sendGet(code);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
         return "index";
+    }
+
+    // HTTP GET request
+    private void sendGet(String code) throws Exception {
+        String url = "https://oauth.vk.com/access_token?" +
+                "client_id=4877168&" +
+                "client_secret=MFA0tWhWgY5SnvO1hSBm&" +
+                "code=" + code + "&" +
+                "redirect_uri=http://vdolgu.mil3.bget.ru";
+
+        System.out.println(url);
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
     }
 
 }
