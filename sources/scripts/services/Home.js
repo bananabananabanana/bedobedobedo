77c7066ebf64d2150bc01ff904939adf0c53d6d5
milomory <y3l0l3k0r>
2015-04-24 13:26:54 +0300

    banana. Phase ibn separate backEnd and frontEnd.

--- sources/scripts/services/Home.js
+++ sources/scripts/services/Home.js
@@ -1,15 +1,16 @@
 function Home(http) {
     this.getUser = function (callback) {
-        var data = [];
-        for (var i = 0; i < 10; i++) {
-            data.push({
-                id: i,
-                name: 'User ' + i
-            });
-        }
-        callback(data);
-        //http.get('')
-        //    .success(callback);
+        //var data = [];
+        //for (var i = 0; i < 10; i++) {
+        //    data.push({
+        //        id: i,
+        //        name: 'User ' + i
+        //    });
+        //}
+        //callback(data);
+        http
+            .get('/home/getData')
+            .success(callback);
     };
 }
 Home.$inject = ['$http'];
