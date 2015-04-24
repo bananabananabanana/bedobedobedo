be158e40ab6d30725b6bc3a878559b00c9458bbb
milomory <y3l0l3k0r>
2015-04-24 18:12:07 +0300

    login + main controllers start

--- src/main/java/rf/vdolgu/controller/MainController.java
+++ src/main/java/rf/vdolgu/controller/MainController.java
@@ -1,114 +1,53 @@
 package rf.vdolgu.controller;
 
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import rf.vdolgu.dao.UserDAO;
 import rf.vdolgu.dao.UserDAOImpl;
 import rf.vdolgu.model.User;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.Serializable;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-@RequestMapping("/")
-public class MainController implements Serializable {
-
-    private final String USER_AGENT = "Mozilla/5.0";
+@RequestMapping("/main")
+public class MainController {
 
     public MainController(){
         System.out.println("MainController");
     }
 
+    UserDAO userDAO = new UserDAOImpl();
 
     @RequestMapping(method = RequestMethod.GET)
     public String index() {
-
         return "index";
     }
 
-    @RequestMapping(method = RequestMethod.GET, params="code")
-    public String getCode(@RequestParam("code") String code) throws Exception {
-        System.out.println(code);
-        if (code != null) {
-            try {
-                String ResponseCode = sendGet(code);
-                System.out.println("print result after sendGet: " + ResponseCode);
-
-                //insert JSON serialaized
-                JSONParser parser = new JSONParser();
-                Object obj = parser.parse(ResponseCode);
-                JSONObject jsonObj = (JSONObject) obj;
-
-                String access_token = jsonObj.get("access_token").toString();
-                String user_id = jsonObj.get("user_id").toString();
-
-                System.out.println("print access_token: " + access_token);
-                System.out.println("print user_id: " + user_id);
-
-                User user = new User();
-                user.setName("banana");
-                user.setAccess_token(access_token);
-                user.setUser_id(new Integer(user_id));
-
-                UserDAO userDAO = new UserDAOImpl();
-                userDAO.insertUser(user);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
+    @RequestMapping(value = "/friend", method = RequestMethod.GET)
+    public String friend() {
         return "index";
     }
 
-    // HTTP GET request
-    private String sendGet(String code) throws Exception {
-        /*String url = "https://oauth.vk.com/access_token?" +
-                "client_id=4877168&" +
-                "client_secret=MFA0tWhWgY5SnvO1hSBm&" +
-                "code=" + code + "&" +
-                "redirect_uri=http://vdolgu.mil3.bget.ru";*/
-        String url = "https://oauth.vk.com/access_token?" +
-                "client_id=4859620&" +
-                "client_secret=7XNY4yQYAfVatUse3ejy&" +
-                "code=" + code + "&" +
-                "redirect_uri=http://xn--b1acc2ao6a.xn--p1ai";
-
-        System.out.println(url);
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-        // optional default is GET
-        con.setRequestMethod("GET");
-
-        //add request header
-        con.setRequestProperty("User-Agent", USER_AGENT);
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        //print result
-        System.out.println("print result: " + response.toString());
-        return response.toString();
+    @RequestMapping(value = "/getData", method = RequestMethod.POST)
+    @ResponseBody
+    public String getData() {
+        //return userDAO.getAllUsers();
+        return "[{" +
+                "                \"id\": 1,\n" +
+                "                \"image\": \"http://placehold.it/40x40\",\n" +
+                "                \"fullname\": \"Друг 1\",\n" +
+                "                \"rating\": 123,\n" +
+                "                \"vkid\": 1639840" +
+                "}]";
     }
 
+/*    @RequestMapping(value = "/{user}", method = RequestMethod.GET)
+    public String getUser(@PathVariable("user") String user) {
+        System.out.println(user);
+        return "index";
+    }*/
 }
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -5,34 +5,18 @@
     <meta name="fragment" content="!">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link href="/libraries/lib.min.css" rel="stylesheet">
     <link href="/application/app.min.css" rel="stylesheet">
     <base href="/">
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-
 </head>
 <body>
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-        <ul class="nav navbar-nav">
-            <li><a ng-href="/">Index</a></li>
-            <li><a ng-href="/home">Home</a></li>
-            <li><a ng-href="/home/john">Home John</a></li>
-        </ul>
-    </div>
-</nav>
 
-Вдолгушечка
+    <ion-nav-view></ion-nav-view>
 
-<div class="container" style="margin-top: 50px;">
-    <section ng-view=""></section>
-</div>
+    <script src="/libraries/lib.min.js"></script>
+    <script src="/application/app.min.js"></script>
 
-<script src="/libraries/lib.min.js"></script>
-<script src="/application/app.min.js"></script>
 </body>
 </html>
